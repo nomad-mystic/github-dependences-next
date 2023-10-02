0e2fec5ab647762393ed862878160b42756ab18f
nomad-mystic <nomadmystics@gmail.com>
2023-10-02 01:06:10 -0700

    kpm: Add Shadcn UI to the project

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import './globals.css'
+import './styles/globals.css'
 
 // Next
 import type {Metadata} from 'next'
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import Card from "@/app/ui/Card/Card";
 
+
 const LoginPage = (): React.JSX.Element => {
     return (
-        <section className="">
-            <Card>
-                <section className="text-teal flex flex-col items-center justify-between bg-cardBackground p-8 mt-0 mb-0 mr-auto mr-left-0 w-fit">
+        <section className="flex flex-col items-center justify-center h-screen">
+            <Card className="w-1/3 flex flex-col items-center justify-between bg-cardBackground">
+                <section className="text-teal p-8 mt-0 mb-0 w-fit">
                     <div className="flex flex-col mb-6">
                         <label htmlFor="username" className="mb-2 font-body">Username</label>
                         <input type="text" id="username"/>
