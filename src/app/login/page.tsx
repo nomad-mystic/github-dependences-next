014020f2cdc31a3d3f9b7d3dc10b47b1033649c8
nomad-mystic <nomadmystics@gmail.com>
2023-10-02 15:48:36 -0700

    kpm: Update package.json and login page component/styles

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,15 +1,13 @@
 "use client"
 
 import React from 'react';
-// import Card from "@/app/ui/Card/Card";
 
+// Styles
+import './login.css';
+
+// @link https://ui.shadcn.com/docs/components/card
 import {
-    Card,
-    CardContent,
-    CardDescription,
-    CardFooter,
-    CardHeader,
-    CardTitle,
+    Card
 } from '@/app/components/ui/card';
 
 import { Button } from '@/app/components/ui/button';
@@ -28,23 +26,38 @@ const LoginPage = (): React.JSX.Element => {
     const { setTheme } = useTheme();
     setTheme('dark');
 
+
+    const onSubmit = (event: React.FormEvent): void => {
+        try {
+            event.preventDefault();
+
+
+        } catch (err: any) {
+            console.log('LoginPage.onSubmit()');
+            console.log(err);
+        }
+    };
+
     return (
-        <section className="flex flex-col items-center justify-center h-screen">
+        <section className="Login flex flex-col items-center justify-center h-screen">
             <Card className="w-1/3 flex flex-col items-center justify-between bg-cardBackground">
                 <section className="text-teal p-8 mt-0 mb-0 w-full">
-                    <div className="flex flex-col mb-6">
-                        <Label htmlFor="username" className="mb-2 font-body">Username</Label>
-                        <Input type="text" id="username" />
-                    </div>
-                    <div className="flex flex-col mb-6">
-                        <Label htmlFor="password" className="mb-2 font-body">Password</Label>
-                        <Input type="password" id="password" />
-                    </div>
-                    <div className="flex justify-center w-1/2 m-auto">
-                        <Button className="font-body w-full">Sign-in</Button>
-                    </div>
+                    <form onSubmit={ onSubmit }>
+                        <div className="flex flex-col mb-6">
+                            <Label htmlFor="username" className="mb-2 font-body">Username</Label>
+                            <Input type="text" id="username" />
+                        </div>
+                        <div className="flex flex-col mb-6">
+                            <Label htmlFor="password" className="mb-2 font-body">Password</Label>
+                            <Input type="password" id="password" />
+                        </div>
+                        <div className="flex justify-center w-1/2 m-auto">
+                            <Button className="font-body w-full">Sign-in</Button>
+                        </div>
+                    </form>
+
                     <div className="flex justify-center w-1/2 m-auto mt-4">
-                        <p className="text-xs">Dont have have an account?</p><a className="text-primary text-xs ml-1">Create One.</a>
+                        <p className="text-xs">Dont have have an account?</p><a className="Login-createAccount text-primary text-xs ml-1">Create One.</a>
                     </div>
                 </section>
             </Card>
