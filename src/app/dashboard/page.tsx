ae509f79b9c907379922a4e51c4df05cf6f3221c
nomad-mystic <nomadmystics@gmail.com>
2023-12-19 16:42:18 -0800

    kpm: Add stub components/page

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,9 +1,45 @@
-import React from 'react';
+'use client';
+
+import React, { useEffect, useState } from 'react';
+
+// Components
+import Sidebar from '@/app/components/sidebar/sidebar';
+
 
 const DashboardPage = () => {
+    const [repos, setRepos] = useState([]);
+    const [isError, setIsError] = useState(false);
+
+    useEffect(() => {
+
+        const callForRepos = async () => {
+            try {
+                const reposRaw = await fetch('/api/v1/repos/all');
+                const reposJson = await reposRaw.json();
+
+                setRepos(reposJson);
+            } catch (err) {
+
+                console.log(err);
+                setIsError(true);
+
+            }
+        };
+
+        callForRepos().catch(err => {
+            console.log(err);
+            setIsError(true);
+        });
+    }, []);
+
+
     return (
         <main>
-            <h1>This is the dashboard</h1>
+            <Sidebar />
+
+            <section>
+
+            </section>
         </main>
     );
 };
